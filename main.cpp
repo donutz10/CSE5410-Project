8a02423fc1eda7a71468d7dea8bc2a54737866c4
donutz10 <rvrt2011@gmail.com>
2022-10-13 12:20:30 -0700

    Kevin's code

---
+++ main.cpp
@@ -0,0 +1,87 @@
+// Arduino-Ardumoto obstacle avoiding robot
+#include <NewPing.h>
+#define TRIGGER_PIN 7    // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN 6       // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum
+
+// sensor distance is rated at 400-1000cm.
+
+#define CW 0
+#define CCW 1
+
+// Motor definitions to make life easier:
+#define MOTOR_A 0
+#define MOTOR_B 1
+// Pin Assignments
+// Don’t change these! These pins are statically defined by the shield layout
+const byte PWMA = 3;                                // PWM control (speed) for motor A
+const byte PWMB = 11;                               // PWM control (speed) for motor B
+const byte DIRA = 12;                               // Direction control for motor A
+const byte DIRB = 13;                               // Direction control for motor B
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum
+
+// distance.
+
+void setup()
+{
+    setupArdumoto(); // Set all pins as outputs
+}
+void loop()
+{
+    delay(50);
+    unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+    if (uS / US_ROUNDTRIP_CM > 50 || uS / US_ROUNDTRIP_CM == 0)
+    {
+        // Move forward
+        forward(255);
+    }
+    else if (uS / US_ROUNDTRIP_CM < 50)
+    {
+        turnRight(255);
+        delay(500);
+    }
+}
+// driveArdumoto drives ’motor’ in direction ’dir’ at speed ’spd’
+void driveArdumoto(byte motor, byte dir, byte spd)
+{
+    if (motor == MOTOR_A)
+    {
+        digitalWrite(DIRA, dir);
+        analogWrite(PWMA, spd);
+    }
+    else if (motor == MOTOR_B)
+    {
+        digitalWrite(DIRB, dir);
+        analogWrite(PWMB, spd);
+    }
+}
+void forward(byte spd) // Runs both motors at speed ’spd’
+{
+    driveArdumoto(MOTOR_A, CW, spd); // Motor A at speed spd
+    driveArdumoto(MOTOR_B, CW, spd); // Motor B at speed spd
+}
+void turnRight(byte spd)
+{
+    stopArdumoto(MOTOR_B);           // Motor B stop
+    driveArdumoto(MOTOR_A, CW, spd); // Motor A run
+}
+// stopArdumoto makes a motor stop
+void stopArdumoto(byte motor)
+{
+    driveArdumoto(motor, 0, 0);
+}
+
+// setupArdumoto initializes all pins
+void setupArdumoto()
+{
+    // All pins should be set up as outputs:
+    pinMode(PWMA, OUTPUT);
+    pinMode(PWMB, OUTPUT);
+    pinMode(DIRA, OUTPUT);
+    pinMode(DIRB, OUTPUT);
+    // Initialize all pins as low:
+    digitalWrite(PWMA, LOW);
+    digitalWrite(PWMB, LOW);
+    digitalWrite(DIRA, LOW);
+    digitalWrite(DIRB, LOW);
+}
